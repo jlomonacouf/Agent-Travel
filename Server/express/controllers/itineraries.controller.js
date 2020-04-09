431e28e821523d971faf0df72fa8043e158d0eb8
jlomonacouf <jlomonaco@ufl.edu>
2020-04-09 00:36:31 -0400

    CRUD for itineraries. Added ability to like and dislike itineraries

--- Server/express/controllers/itineraries.controller.js
+++ Server/express/controllers/itineraries.controller.js
@@ -1,8 +1,62 @@
 var con = require("../db");
 
+exports.getUserItineraries = (req, res) =>
+{
+    if(req.session.loggedin === false || req.session.loggedin === undefined)
+        return res.json({success: false, message: "Not authorized"});
+
+    con.query('SELECT id FROM Users WHERE username = ?', [req.params.username], function(err, idResults) 
+    {
+        if(err)
+            return res.json({success: false, message: "Error getting user itineraries"})
+        
+        if(idResults.length !== 0)
+        {
+            con.query('SELECT * FROM Itineraries WHERE user_id = ?', [idResults[0].id], function(err, results) 
+            {
+                if(err)
+                    return res.json({success: false, message: "Error getting user itineraries"})
+                
+                return res.json({success: true, results});
+            })
+        }
+        else
+            return res.json({success: false, message: "User not found"})
+    })
+}
+
+exports.getAllItineraries = (req, res) => 
+{
+    if(req.session.loggedin === false || req.session.loggedin === undefined)
+        return res.json({success: false, message: "Not authorized"});
+
+    con.query('SELECT * FROM Itineraries', function(err, results) 
+    {
+        if(err)
+            return res.json({success: false, message: "Error getting user itineraries"})
+        
+        return res.json({success: true, results});
+    })
+}
+
+
+exports.getItineraryByID = (req, res) =>
+{
+    if(req.session.loggedin === false || req.session.loggedin === undefined)
+        return res.json({success: false, message: "Not authorized"});
+
+    con.query('SELECT * FROM Itineraries WHERE id = ?', [req.params.id], function(err, results) 
+    {
+        if(err)
+            return res.json({success: false, message: "Error getting user itineraries"})
+        
+        return res.json({success: true, results});
+    })
+}
+
 exports.createItinerary = (req, res) =>
 {
-    if(!req.session.loggedin)
+    if(req.session.loggedin === false || req.session.loggedin === undefined)
         return res.json({success: false, message: "Not authorized"});
 
     var itinerary = {
@@ -14,6 +68,12 @@ exports.createItinerary = (req, res) =>
         city: req.body.city,
         region: req.body.region,
     }
+
+    if(req.body.start_date)
+        itinerary.start_date = req.body.start_date;
+    if(req.body.end_date)
+        itinerary.end_date = req.body.end_date;
+
 
     //Fail cases:
     if(text === "")
@@ -32,20 +92,20 @@ exports.createItinerary = (req, res) =>
 
 exports.deleteItinerary = (req, res) =>
 {
-    if(!req.session.loggedin)
+    if(req.session.loggedin === false || req.session.loggedin === undefined)
         return res.json({success: false, message: "Not authorized"});
 
     var userid = req.session.userid;
     var itineraryID = req.body.itineraryID;
 
-    con.query("SELECT user_id FROM Itineraries WHERE id = ?", itineraryID, function(err, results)
+    con.query("SELECT user_id FROM Itineraries WHERE id = ?", [itineraryID], function(err, results)
     {
         if(err)
             return res.json({success: false, message: "Error deleting itinerary"});
 
         if(results[0].user_id === userid)
         {
-            con.query("DELETE FROM Itineraries WHERE id = ?", itineraryID, function(err) 
+            con.query("DELETE FROM Itineraries WHERE id = ?", [itineraryID], function(err) 
             {
                 if(err)
                     return res.json({success: false, message: "Error deleting itinerary"});
@@ -64,9 +124,50 @@ exports.deleteItinerary = (req, res) =>
     con.commit();
 }
 
+exports.likeItinerary = (req, res) =>
+{
+    if(req.session.loggedin === false || req.session.loggedin === undefined)
+        return res.json({success: false, message: "Not authorized"})
+    
+    var likes = {
+        user_id: req.session.userid,
+        itinerary_id: req.body.itinerary_id
+    }
+    
+    con.query("INSERT INTO Likes_Itineraries SET ? ", [likes], function(err) 
+    {
+        if(err)
+        {
+            console.log(err);
+            return res.json({success: false, message: "Unable to like itinerary"});
+        }
+
+        con.commit();
+
+        return res.json({success: true, message: "Successfully liked itinerary"});
+    });
+}
+
+exports.dislikeItinerary = (req, res) =>
+{
+    if(req.session.loggedin === false || req.session.loggedin === undefined)
+        return res.json({success: false, message: "Not authorized"})
+    
+    con.query("DELETE FROM Likes_Itineraries WHERE user_id = ? AND itinerary_id = ?", [req.session.userid, req.body.itinerary_id], function(err) 
+    {
+        if(err)
+            return res.json({success: false, message: "Unable to dislike itinerary"});
+
+        con.commit();
+
+        return res.json({success: true, message: "Successfully disliked itinerary"});
+    });
+}
+
+
 exports.uploadFile = (req, res) =>
 {
-    if(!req.session.loggedin)
+    if(req.session.loggedin === false || req.session.loggedin === undefined)
         return res.json({success: false, message: "Not authorized"})
 
     var params = {
--- Server/express/routes/itineraries.routes.js
+++ Server/express/routes/itineraries.routes.js
@@ -3,8 +3,29 @@ var router = express.Router();
 
 var itinerary = require('../controllers/itineraries.controller')
 
-router.post('/create-itinerary', itinerary.createItinerary)
+router.get('/get/:username', itinerary.getUserItineraries); 
+/* No parameters - Returns all trips created by a user */
 
-router.post('/delete-itinerary', itinerary.deleteItinerary)
+router.get('/get-all-itineraries', itinerary.getAllItineraries); 
+/* No parameters - Returns all trips in the database */
+
+router.get('/get-by-id/:id', itinerary.getItineraryByID); 
+/* No parameters - Returns all trips in the database */
+
+router.post('/create-itinerary', itinerary.createItinerary);
+
+router.post('/delete-itinerary', itinerary.deleteItinerary);
+
+router.post('/like-itinerary', itinerary.likeItinerary);
+/*
+Parameters
+itinerary_id - int
+*/
+
+router.post('/dislike-itinerary', itinerary.dislikeItinerary);
+/*
+Parameters
+itinerary_id - int
+*/
 
 module.exports = router;
